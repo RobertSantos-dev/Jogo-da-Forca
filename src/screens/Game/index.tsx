131ef7d58bb4719b1a25ba41a096923b206937f0
Robert Santos <robertdosantos09@gmail.com>
2023-05-03 20:57:15 -0300

    chore: Adicionando um arquivo json, com todas as palavras a serem usadas.

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,9 +1,24 @@
 import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { mobile } from '../../services/words.json';
+
+type wordType = {
+  name: string,
+  desc: string,
+}
 
 export default function Game() {
+  const [word, setWord] = useState<wordType>({ name: "", desc: "" });
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * mobile.length);
+    setWord(mobile[index]);
+  }, [])
+
   return (
     <View>
-      <Text>Game screen</Text>
+      <Text>{ word.name }</Text>
+      <Text>{ word.desc }</Text>
     </View>
   );
 }
